a26f1061113e45adf7836a9fbc31adcdb92e5f72
AmrutaNichale <amrutanichale@gmail.com>
2026-05-26 13:16:09 +0530

    Completed authentication APIs

---
+++ Desktop/TranceVista/backend/trancevista/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using BCrypt.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TranceVista.Data;
+using TranceVista.DTOs;
+using TranceVista.Models;
+
+namespace TranceVista.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // REGISTER
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var existingUser = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email == dto.Email);
+
+            if (existingUser != null)
+            {
+                return BadRequest("Email already exists");
+            }
+
+            var user = new User
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+            };
+
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return Ok("User registered successfully");
+        }
+
+        // LOGIN
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email == dto.Email);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            bool isPasswordValid =
+                BCrypt.Net.BCrypt.Verify(dto.Password, user.PasswordHash);
+
+            if (!isPasswordValid)
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            return Ok("Login successful");
+        }
+    }
+}
